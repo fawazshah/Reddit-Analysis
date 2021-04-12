353d5864babb09f136cff17656668aea41d97891
Fawaz Shah <fawaz010@gmail.com>
2021-04-12 20:42:52 +0100

    get comment tree

--- get.py
+++ get.py
@@ -1,6 +1,16 @@
 import praw
+from pprint import pprint
 
 reddit = praw.Reddit("bias-bot", user_agent="bias-detection:v1.0 (by Fawaz Shah)")
 
 liberal = reddit.subreddit("liberal")
-print(liberal.description)
+
+for submission in liberal.top(limit=100):
+    print(submission.title)
+    print(submission.url)
+
+    # Resolves instances of MoreComments in comment tree
+    submission.comments.replace_more()
+
+    comment_tree = submission.comments.list()
+    pprint(len(comment_tree))
