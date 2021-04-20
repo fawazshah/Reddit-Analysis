85a96e2225c5e5ff87bf699145eec01194f78acd
Fawaz Shah <fawaz010@gmail.com>
2021-04-20 01:45:23 +0100

    Check for liberal submissions

--- get_pushshift_data.py
+++ get_pushshift_data.py
@@ -1,15 +1,19 @@
 import json
+
 from more_itertools import chunked
 
 batch_size = 100000
 
-with open('pushshift_data/comments/2019-12-15.json', 'r') as f:
+with open('pushshift_data/submissions/2013-09.json', 'r') as f:
 
     # Since file sizes are extremely large, we split into batches
     chunks = list(chunked(f, batch_size))
 
     for chunk in chunks:
-        print(len(chunk))
-        comments = []
-        for json_obj in chunk:
-            comments.append(json.loads(json_obj))
+        for submission_json in chunk:
+
+            # Converting comment from JSON into dict
+            submission = json.loads(submission_json)
+
+            if submission['subreddit'].lower() == "liberal":
+                print(json.dumps(submission))
