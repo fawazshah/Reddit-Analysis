d5a67cf9b2c5237b86e8a16c18caa7f683250ffd
Fawaz Shah <fawaz010@gmail.com>
2021-04-22 19:09:11 +0100

    Add error handling for download/parsing. print article info

--- get_praw.py
+++ get_praw.py
@@ -13,23 +13,26 @@ num_articles = 0
 num_comments = 0
 
 for submission in liberal.hot(limit=1000):
-    print(submission.title)
-    print(submission.url)
-    print(submission.score)
+    print(f"\nArticle headline: {submission.title}")
+    print(f"Article score: {submission.score}")
 
     # Scraping article content
     article = Article(submission.url)
-    article.download()
-    article.parse()
-    print(article.text)
+    try:
+        article.download()
+        article.parse()
+    except Exception as err:
+        print(err)
+        print("continuing...")
+        continue
 
     # Resolves instances of MoreComments in comment tree
     submission.comments.replace_more()
 
     comment_list = submission.comments.list()
-    print(len(comment_list))
+    print(f"Number of comments: {len(comment_list)}")
     num_comments += len(comment_list)
     num_articles += 1
 
-    print(f"Number of articles so far: {num_articles}")
+    print(f"\nNumber of articles so far: {num_articles}")
     print(f"Number of comments so far: {num_comments}")
