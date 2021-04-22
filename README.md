d394da24a193e4d11d9daf6e66224064e639cda0
Fawaz Shah <fawaz010@gmail.com>
2021-04-22 16:12:38 +0100

    Pull Reddit post article bodies using newspaper package.
    Move vars() dump to readme. Experiment more with pushshift api

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Reddit Political Bias Analysis
 
 Part of my Masters' thesis work at Imperial College London.
+
+### Determining available attributes of an object
+
+Especially useful for objects from the Reddit API where attributes aren't
+well documented.
+
+To print all object attribute names + values in json format:
+
+`print(json.dumps(vars(obj), default=lambda o: str(o)))`
+
+use `dir()` if obj has no `__dict__` attribute
--- get_praw.py
+++ get_praw.py
@@ -1,20 +1,35 @@
 import json
 import praw
+from newspaper import Article
 from pprint import pprint
 
-# To print all object attribute names + values in json format
-# print(json.dumps(vars(obj), default=lambda o: str(o)))
 
 reddit = praw.Reddit("bias-bot", user_agent="bias-detection:v1.0 (by Fawaz Shah)")
 
 liberal = reddit.subreddit("liberal")
 
-for submission in liberal.top(limit=100):
+
+num_articles = 0
+num_comments = 0
+
+for submission in liberal.hot(limit=1000):
     print(submission.title)
     print(submission.url)
+    print(submission.score)
+
+    # Scraping article content
+    article = Article(submission.url)
+    article.download()
+    article.parse()
+    print(article.text)
 
     # Resolves instances of MoreComments in comment tree
     submission.comments.replace_more()
 
     comment_list = submission.comments.list()
-    comment = comment_list[0]
+    print(len(comment_list))
+    num_comments += len(comment_list)
+    num_articles += 1
+
+    print(f"Number of articles so far: {num_articles}")
+    print(f"Number of comments so far: {num_comments}")
