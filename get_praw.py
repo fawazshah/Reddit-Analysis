3722fa9a4af66422ff7727734032bd00644a0930
Fawaz Shah <fawaz010@gmail.com>
2021-04-25 00:10:15 +0100

    Compare domain name of article to ground truth data domain names

--- get_praw.py
+++ get_praw.py
@@ -2,13 +2,13 @@ import json
 import praw
 from newspaper import Article
 import pandas as pd
+from urllib.parse import urlparse
 
 
 reddit = praw.Reddit("bias-bot", user_agent="bias-detection:v1.0 (by Fawaz Shah)")
 
 articles_df = pd.DataFrame(columns=['submission id', 'article headline', 'article body', 'bias'])
 comments_df = pd.DataFrame(columns=['comment id', 'submission id', 'comment body', 'bias'])
-
 ground_truths_df = pd.read_csv('data/corpus-balanced-classes.tsv', sep='\t')
 
 liberal = reddit.subreddit("liberal")
@@ -20,15 +20,20 @@ for submission in liberal.top("year", limit=1000):
 
     # Only select posts that are link posts and have score > 10
     if submission.selftext == "" and submission.score > 10:
-        print(f"\nArticle headline: {submission.title}")
-        print(f"Article score: {submission.score}")
+
+        article_domain = urlparse(submission.url).netloc
+        res = ground_truths_df.loc[ground_truths_df['domain'] == article_domain]
+        print(res.empty)
+
+
+        #print(f"\nArticle headline: {submission.title}")
 
         # Scraping article content
         article = Article(submission.url)
         try:
             article.download()
             article.parse()
-            print(article.text)
+            #print(article.text)
         except Exception as err:
             print(err)
             print("continuing...")
@@ -38,9 +43,9 @@ for submission in liberal.top("year", limit=1000):
         submission.comments.replace_more()
 
         comment_list = submission.comments.list()
-        print(f"Number of comments: {len(comment_list)}")
+
         num_comments += len(comment_list)
         num_articles += 1
 
-        print(f"\nNumber of articles so far: {num_articles}")
-        print(f"Number of comments so far: {num_comments}")
+        #print(f"\nNumber of articles so far: {num_articles}")
+        #print(f"Number of comments so far: {num_comments}")
