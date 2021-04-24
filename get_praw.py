27f02bccc660ff28592a43b61c0bf72bc7ad8c08
Fawaz Shah <fawaz010@gmail.com>
2021-04-24 23:04:09 +0100

    Use top from this year instead of hot. Create dfs

--- get_praw.py
+++ get_praw.py
@@ -1,38 +1,46 @@
 import json
 import praw
 from newspaper import Article
-from pprint import pprint
+import pandas as pd
 
 
 reddit = praw.Reddit("bias-bot", user_agent="bias-detection:v1.0 (by Fawaz Shah)")
 
-liberal = reddit.subreddit("liberal")
+articles_df = pd.DataFrame(columns=['submission id', 'article headline', 'article body', 'bias'])
+comments_df = pd.DataFrame(columns=['comment id', 'submission id', 'comment body', 'bias'])
 
+ground_truths_df = pd.read_csv('data/corpus-balanced-classes.tsv', sep='\t')
+
+liberal = reddit.subreddit("liberal")
 
 num_articles = 0
 num_comments = 0
 
-for submission in liberal.hot(limit=1000):
-    print(f"\nArticle headline: {submission.title}")
-    print(f"Article score: {submission.score}")
+for submission in liberal.top("year", limit=1000):
 
-    # Scraping article content
-    article = Article(submission.url)
-    try:
-        article.download()
-        article.parse()
-    except Exception as err:
-        print(err)
-        print("continuing...")
-        continue
+    # Only select posts that are link posts and have score > 10
+    if submission.selftext == "" and submission.score > 10:
+        print(f"\nArticle headline: {submission.title}")
+        print(f"Article score: {submission.score}")
 
-    # Resolves instances of MoreComments in comment tree
-    submission.comments.replace_more()
+        # Scraping article content
+        article = Article(submission.url)
+        try:
+            article.download()
+            article.parse()
+            print(article.text)
+        except Exception as err:
+            print(err)
+            print("continuing...")
+            continue
 
-    comment_list = submission.comments.list()
-    print(f"Number of comments: {len(comment_list)}")
-    num_comments += len(comment_list)
-    num_articles += 1
+        # Resolves instances of MoreComments in comment tree
+        submission.comments.replace_more()
 
-    print(f"\nNumber of articles so far: {num_articles}")
-    print(f"Number of comments so far: {num_comments}")
+        comment_list = submission.comments.list()
+        print(f"Number of comments: {len(comment_list)}")
+        num_comments += len(comment_list)
+        num_articles += 1
+
+        print(f"\nNumber of articles so far: {num_articles}")
+        print(f"Number of comments so far: {num_comments}")
