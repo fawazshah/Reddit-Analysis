08abb562dd0a6f36ae18ae0425e31db7b32d8815
Fawaz Shah <fawaz010@gmail.com>
2021-04-25 00:22:36 +0100

    Remove urllib parsing. Add basic list of subreddits for which we know ground truth biases

--- get_praw.py
+++ get_praw.py
@@ -2,7 +2,6 @@ import json
 import praw
 from newspaper import Article
 import pandas as pd
-from urllib.parse import urlparse
 
 
 reddit = praw.Reddit("bias-bot", user_agent="bias-detection:v1.0 (by Fawaz Shah)")
@@ -11,41 +10,44 @@ articles_df = pd.DataFrame(columns=['submission id', 'article headline', 'articl
 comments_df = pd.DataFrame(columns=['comment id', 'submission id', 'comment body', 'bias'])
 ground_truths_df = pd.read_csv('data/corpus-balanced-classes.tsv', sep='\t')
 
-liberal = reddit.subreddit("liberal")
+subreddits = [
+    (reddit.subreddit("liberal"), 'left'),
+    (reddit.subreddit("democrats"), 'left'),
+    (reddit.subreddit("conservative"), 'right'),
+    (reddit.subreddit("republicans"), 'right'),
+]
 
 num_articles = 0
 num_comments = 0
 
-for submission in liberal.top("year", limit=1000):
+for subreddit, bias in subreddits:
+    for submission in subreddit.top("year", limit=1000):
 
-    # Only select posts that are link posts and have score > 10
-    if submission.selftext == "" and submission.score > 10:
-
-        article_domain = urlparse(submission.url).netloc
-        res = ground_truths_df.loc[ground_truths_df['domain'] == article_domain]
-        print(res.empty)
+        # Only select posts that are link posts and have score > 10
+        if submission.selftext == "" and submission.score > 10:
 
 
-        #print(f"\nArticle headline: {submission.title}")
 
-        # Scraping article content
-        article = Article(submission.url)
-        try:
-            article.download()
-            article.parse()
-            #print(article.text)
-        except Exception as err:
-            print(err)
-            print("continuing...")
-            continue
+            #print(f"\nArticle headline: {submission.title}")
 
-        # Resolves instances of MoreComments in comment tree
-        submission.comments.replace_more()
+            # Scraping article content
+            article = Article(submission.url)
+            try:
+                article.download()
+                article.parse()
+                #print(article.text)
+            except Exception as err:
+                print(err)
+                print("continuing...")
+                continue
 
-        comment_list = submission.comments.list()
+            # Resolves instances of MoreComments in comment tree
+            submission.comments.replace_more()
 
-        num_comments += len(comment_list)
-        num_articles += 1
+            comment_list = submission.comments.list()
 
-        #print(f"\nNumber of articles so far: {num_articles}")
-        #print(f"Number of comments so far: {num_comments}")
+            num_comments += len(comment_list)
+            num_articles += 1
+
+            #print(f"\nNumber of articles so far: {num_articles}")
+            #print(f"Number of comments so far: {num_comments}")
