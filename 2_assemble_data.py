5d4443d07ef210c7cac9ac907d45612b0bc53a27
Fawaz Shah <fawaz010@gmail.com>
2021-05-04 01:14:47 +0100

    Reformatting

--- 2_assemble_data.py
+++ 2_assemble_data.py
@@ -2,14 +2,20 @@ import pandas as pd
 from newspaper import Article
 
 subreddit_bias_map = {
-    'liberal': 'left',
-    'democrats': 'left',
-    'conservative': 'right',
-    'republicans': 'right'
+    "liberal": "left",
+    "democrats": "left",
+    "conservative": "right",
+    "republicans": "right",
 }
 
-submissions = pd.read_csv('data/raw-reddit-responses/submissions_top300_year_liberal_democrats_conservative_republicans.tsv', sep='\t')
-comments = pd.read_csv('data/raw-reddit-responses/comments_top300_year_liberal_democrats_conservative_republicans.tsv', sep='\t')
+submissions = pd.read_csv(
+    "data/raw-reddit-responses/submissions_top300_year_liberal_democrats_conservative_republicans.tsv",
+    sep="\t",
+)
+comments = pd.read_csv(
+    "data/raw-reddit-responses/comments_top300_year_liberal_democrats_conservative_republicans.tsv",
+    sep="\t",
+)
 
 num_submissions = len(submissions)
 num_comments = len(comments)
@@ -23,44 +29,47 @@ comments_df = pd.DataFrame(
 
 for i, submission in submissions.iterrows():
 
-    print(f"Submission {i} / {num_submissions}")
+    print(f"Submission {i+1} / {num_submissions}")
 
     # Scraping article content
-    article = Article(submission['url'])
+    article = Article(submission["url"])
     try:
         article.download()
         article.parse()
     except Exception as err:
         continue
-    
-    ground_truth_bias = subreddit_bias_map[submission['subreddit'].lower()]
+
+    ground_truth_bias = subreddit_bias_map[submission["subreddit"].lower()]
 
     new_row = {
-        "submission id": submission['id'],
-        "article headline": submission['title'],
+        "submission id": submission["id"],
+        "article headline": submission["title"],
         "article body": article.text,
         "bias": ground_truth_bias,
     }
     submissions_df = submissions_df.append(new_row, ignore_index=True)
-    submissions_df.to_csv('data/assembled-data/submissions_top300_year_liberal_democrats_conservative_republicans.tsv', sep='\t', index=False)
+    submissions_df.to_csv(
+        "data/assembled-data/submissions_top300_year_liberal_democrats_conservative_republicans.tsv",
+        sep="\t",
+        index=False,
+    )
 
-    if (i % 10 == 0):
-        print(i)
 
 for i, comment in comments.iterrows():
 
-    print(f"Comment {i} / {num_comments}")
+    print(f"Comment {i+1} / {num_comments}")
 
-    ground_truth_bias = subreddit_bias_map[comment['subreddit'].lower()]
+    ground_truth_bias = subreddit_bias_map[comment["subreddit"].lower()]
 
     new_row = {
-        'comment id': comment['id'],
-        'submission id': comment['_submission'],
-        'comment body': comment['body'],
-        'bias': ground_truth_bias,
+        "comment id": comment["id"],
+        "submission id": comment["_submission"],
+        "comment body": comment["body"],
+        "bias": ground_truth_bias,
     }
     comments_df = comments_df.append(new_row, ignore_index=True)
-    comments_df.to_csv('data/assembled-data/comments_top300_year_liberal_democrats_conservative_republicans.tsv', sep='\t', index=False)
-
-    if (i % 10 == 0):
-        print(i)
+    comments_df.to_csv(
+        "data/assembled-data/comments_top300_year_liberal_democrats_conservative_republicans.tsv",
+        sep="\t",
+        index=False,
+    )
