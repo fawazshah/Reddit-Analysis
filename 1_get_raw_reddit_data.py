019e436c809e56aaa2eec9d424f8a810fbd12ee9
Fawaz Shah <fawaz010@gmail.com>
2021-05-11 00:05:38 +0100

    New rule to only collect max 100 comments per submission

--- 1_get_raw_reddit_data.py
+++ 1_get_raw_reddit_data.py
@@ -10,8 +10,10 @@ comments_df = pd.DataFrame()
 reddit = praw.Reddit("bias-bot", user_agent="bias-detection:v1.0 (by Fawaz Shah)")
 
 subreddits = [
-    reddit.subreddit("libertarian"),
-    reddit.subreddit("sandersforpresident"),
+    reddit.subreddit("liberal"),
+    reddit.subreddit("conservative"),
+    reddit.subreddit("democrats"),
+    reddit.subreddit("republican")
 ]
 
 for subreddit in subreddits:
@@ -30,12 +32,19 @@ for subreddit in subreddits:
             submission.comments.replace_more()
 
             comment_list = submission.comments.list()
+
+            num_comments_collected = 0
             for comment in comment_list:
 
                 # Only select comments with score > 10
                 if comment.score > 10:
                     comment_attributes = vars(comment)
                     comments_df = comments_df.append(comment_attributes, ignore_index=True)
+                    num_comments_collected += 1
+                
+                # Collect max 100 comments per submission
+                if num_comments_collected > 100:
+                    break
             print(f"Submission {len(submissions_df)}, number of comments: {len(comments_df)}", end='\r')
     
         # Save data to file after every submission
