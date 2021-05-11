f4bb1a2b05e6cbee1637745cd8181fe49afb7496
Fawaz Shah <fawaz010@gmail.com>
2021-05-11 12:56:12 +0100

    Add scripts to check integrity of submission ids

---
+++ check_ids_assembled.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+submissions_df = pd.read_csv('data/assembled-data/submissions_top300_year_republican.tsv', sep='\t')
+comments_df = pd.read_csv('data/assembled-data/comments_top300_year_republican.tsv', sep='\t')
+
+print(len(submissions_df))
+print(len(comments_df))
+
+submission_ids = list(submissions_df['submission id'])
+for i, row in comments_df.iterrows():
+    if row['submission id'] not in submission_ids:
+        print("hellooo")
---
+++ check_ids_raw.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+submissions_df = pd.read_csv('data/raw-reddit-responses/submissions_top300_year_republican.tsv', sep='\t')
+comments_df = pd.read_csv('data/raw-reddit-responses/comments_top300_year_republican.tsv', sep='\t')
+
+print(len(submissions_df))
+print(len(comments_df))
+
+submission_ids = list(submissions_df['id'])
+for i, row in comments_df.iterrows():
+    if row['_submission'] not in submission_ids:
+        print("hellooo")
